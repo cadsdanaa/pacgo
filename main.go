e92884dae789fb43675d8356be85dd6030f2503d
Alex Dana <cadsdanaa@gmail.com>
2020-08-21 16:20:50 -0500

    Step 2 - Terminal accepts basic input. Terminal cleans on input.

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/danicat/simpleansi"
 	"log"
 	"os"
+	"os/exec"
 )
 
 var maze []string
 
 func main() {
+	initialize()
+	defer cleanup()
+
 	err := loadMaze("resources/maze01.txt")
 	if err != nil {
 		log.Fatal("Failed to load maze")
@@ -17,8 +22,51 @@ func main() {
 	}
 	for {
 		printMaze()
-		break
+
+		input, err := readInput()
+		if err != nil {
+			log.Fatal("Failed to read input")
+			break
+		}
+
+		if input == "ESC" {
+			break
+		}
 	}
+}
+
+func initialize() {
+	cbTerm := exec.Command("stty", "cbreak", "-echo")
+	cbTerm.Stdin = os.Stdin
+
+	err := cbTerm.Run()
+	if err != nil {
+		log.Fatal("Unable to initialize terminal")
+	}
+}
+
+func cleanup() {
+	cookedTerminal := exec.Command("stty", "-cbreak", "echo")
+	cookedTerminal.Stdin = os.Stdin
+
+	err := cookedTerminal.Run()
+	if err != nil {
+		log.Fatal("Unable to restore terminal")
+	}
+}
+
+func readInput() (string, error) {
+	buffer := make([]byte, 100)
+	cnt, err := os.Stdin.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+
+	if cnt == 1 && buffer[0] == 0x1b {
+		return "ESC", nil
+	}
+
+	return "", nil
 }
 
 func loadMaze(file string) error {
@@ -37,6 +85,7 @@ func loadMaze(file string) error {
 }
 
 func printMaze() {
+	simpleansi.ClearScreen()
 	for _, line := range maze {
 		fmt.Println(line)
 	}
