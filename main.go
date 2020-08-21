7e498aeb013eed2b76013002b825c749bb2683db
Alex Dana <cadsdanaa@gmail.com>
2020-08-21 14:45:49 -0500

    Step 1 - Added loading and printing of maze

--- main.go
+++ main.go
@@ -1,10 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+var maze []string
 
 func main() {
+	err := loadMaze("resources/maze01.txt")
+	if err != nil {
+		log.Fatal("Failed to load maze")
+		return
+	}
 	for {
-		fmt.Println("Hello, Pac Go!")
+		printMaze()
 		break
+	}
+}
+
+func loadMaze(file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		maze = append(maze, line)
+	}
+	return nil
+}
+
+func printMaze() {
+	for _, line := range maze {
+		fmt.Println(line)
 	}
 }
