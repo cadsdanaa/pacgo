9ce627a93a41ffa0a04ea289b0d3d7b21f5f89c4
Alex Dana <cadsdanaa@gmail.com>
2020-08-24 11:56:41 -0500

    Step 6 - Movement now nonblocking

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 )
 
 func main() {
@@ -13,16 +14,31 @@ func main() {
 		log.Fatal("Failed to load maze")
 		return
 	}
+
+	input := make(chan string)
+	go func(ch chan<- string) {
+		for {
+			input, err := ReadInput()
+			if err != nil {
+				log.Fatal("Failed to read input")
+				ch <- "ESC"
+			}
+			ch <- input
+		}
+	}(input)
+
 	for {
 		PrintMaze()
 
-		input, err := ReadInput()
-		if err != nil {
-			log.Fatal("Failed to read input")
-			break
+		select {
+		case inp := <-input:
+			if inp == "ESC" {
+				Lives = 0
+			}
+			MovePlayer(inp)
+		default:
 		}
 
-		MovePlayer(input)
 		MoveGhosts()
 
 		for _, ghost := range ghosts {
@@ -31,8 +47,10 @@ func main() {
 			}
 		}
 
-		if input == "ESC" || Lives <= 0 || Dots == 0 {
+		if Lives <= 0 || Dots == 0 {
 			break
 		}
+
+		time.Sleep(200 * time.Millisecond)
 	}
 }
